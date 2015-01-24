6b450c71c08a5c42ef47843d1c5c87125ffad0bb
unknown <oumoulousmane@gmail.com>
2015-01-24 18:46:10 +0100

    Mes Premiers Ajouts

---
+++ App.java
@@ -0,0 +1,35 @@
+package fr.Istic.sir.tpmaven;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+    /**
+     * Commentaire le groupeid est lié aux informations de l'entreprise
+     * d'hanitude à son site
+     */
+    
+    
+    /*
+     * Ajout de code mort: exemple d'une méthode non utlisée
+     */
+    public void afficher( String text){
+    	String t=text;
+    	String s;
+    	System.out.println("Vous voulez afficher "+t);
+    	
+    	
+    	    }
+    
+}
